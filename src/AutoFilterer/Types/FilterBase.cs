c0e911a1ae75ad9bda0e7635ff7a8540d82f29b2
enisn <iletisim@enisnecipoglu.com>
2019-07-11 00:31:01 +0300

    Seperated Filterbase to Generic & non-generic

--- src/AutoFilterer/Types/FilterBase.cs
+++ src/AutoFilterer/Types/FilterBase.cs
@@ -11,9 +11,13 @@ using System.Threading.Tasks;
 
 namespace AutoFilterer.Types
 {
-    public class FilterBase<TEntity> : IFilterableType
+    public class FilterBase : IFilterableType
     {
-        public IQueryable<TEntity> ApplyFilterTo(IQueryable<TEntity> query)
+        /// <summary>
+        /// Automaticly Applies Where() to IQuaryable collection. Please visit project site for more information and customizations.
+        /// </summary>
+        /// <param name="query"></param>
+        public IQueryable<TEntity> ApplyFilterTo<TEntity>(IQueryable<TEntity> query)
         {
             var _type = this.GetType();
             foreach (var entityProperty in typeof(TEntity).GetProperties())
@@ -55,7 +59,13 @@ namespace AutoFilterer.Types
             return query;
         }
 
-        public Expression<Func<TModel, bool>> BuildExpression<TModel>(PropertyInfo property, object value)
+        /// <summary>
+        /// Generates a LINQ Expression with properties
+        /// </summary>
+        /// <typeparam name="TModel">Expression to apply model</typeparam>
+        /// <param name="property">Reflection <see cref="PropertyInfo"/> to filter</param>
+        /// <param name="value">value to filter</param>
+        public virtual Expression<Func<TModel, bool>> BuildExpression<TModel>(PropertyInfo property, object value)
         {
             var parameter = Expression.Parameter(property.DeclaringType, property.Name);
 
@@ -65,6 +75,55 @@ namespace AutoFilterer.Types
                                 );
 
             return Expression.Lambda<Func<TModel, bool>>(comparison, parameter);
+        }
+    }
+
+    public class FilterBase<TEntity> : FilterBase
+    {
+        /// <summary>
+        /// Automaticly Applies Where() to IQuaryable collection. Please visit project site for more information and customizations.
+        /// </summary>
+        /// <param name="query"></param>
+        public IQueryable<TEntity> ApplyFilterTo(IQueryable<TEntity> query)
+        {
+            var _type = this.GetType();
+            foreach (var entityProperty in typeof(TEntity).GetProperties())
+            {
+                try
+                {
+                    var filterProperty = _type.GetProperty(entityProperty.Name);
+                    if (filterProperty == null)
+                        continue;
+
+                    var val = filterProperty.GetValue(this);
+                    if (val == null)
+                        continue;
+
+                    var attribute = filterProperty.GetCustomAttribute<FilteringOptionsBaseAttribute>(false);
+
+                    if (attribute != null)
+                    {
+                        query = query.Where(attribute.BuildExpression<TEntity>(entityProperty, val));
+                        continue;
+                    }
+
+                    if (val is IFilterableType filterableProperty)
+                    {
+                        var expression = filterableProperty.BuildExpression<TEntity>(entityProperty, val);
+                        query = query.Where(expression);
+                    }
+                    else
+                    {
+                        var expression = BuildExpression<TEntity>(entityProperty, val);
+                        query = query.Where(expression);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex?.ToString());
+                }
+            }
+            return query;
         }
     }
 
