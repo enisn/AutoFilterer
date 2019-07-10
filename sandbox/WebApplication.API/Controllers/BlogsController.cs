fe138522bdc578b11df4e1d30bbf95e635abd6a8
enisn <iletisim@enisnecipoglu.com>
2019-07-10 23:17:31 +0300

    Added Sample project

---
+++ sandbox/WebApplication.API/Controllers/BlogsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.API.Dtos;
+using WebApplication.API.Repository;
+
+namespace WebApplication.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        // /api/Blogs
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]BlogFilterDto filter)
+        {
+            var db = new BlogDummyData();
+
+            var list = filter.ApplyFilterTo(db.Blogs).ToList();
+
+            return Ok(list);
+        }
+    }
+}
---
+++ sandbox/WebApplication.API/Dtos/BlogFilterDto.cs
@@ -0,0 +1,27 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Enums;
+using AutoFilterer.Types;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.API.Models;
+
+namespace WebApplication.API.Dtos
+{
+    public class BlogFilterDto : FilterBase<Blog>
+    {
+        public int? CategoryId { get; set; }
+
+        public Range<int> Priority { get; set; }
+
+        [StringFilterOptions(StringFilterOption.Contains)]
+        public string Title { get; set; }
+
+        public bool? IsPublished { get; set; }
+
+        public Range<DateTime> PublishDate { get; set; }
+    }
+}
