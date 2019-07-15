46db2ff6a9d176dba8e3be46c9511134ed3393a1
enisn <iletisim@enisnecipoglu.com>
2019-07-15 18:37:22 +0300

    Added Exceptions in ToPaged extension method

--- src/AutoFilterer/Extensions/QueryExtensions.cs
+++ src/AutoFilterer/Extensions/QueryExtensions.cs
@@ -9,9 +9,23 @@ namespace AutoFilterer.Extensions
     public static class QueryExtensions
     {
         public static IQueryable<T> ToPaged<T>(this IOrderedQueryable<T> source, int page, int pageSize)
-            => source.Skip((page - 1) * pageSize).Take(pageSize);
+        {
+            if (page <= 0)
+                throw new ArgumentOutOfRangeException(nameof(page), "The given parameter can not be zero or negative.");
+            if (page <= 0)
+                throw new ArgumentOutOfRangeException(nameof(pageSize), "The given parameter can not be zero or negative.");
+
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
 
         public static IQueryable<T> ToPaged<T>(this IQueryable<T> source, int page, int pageSize)
-            => (source as IOrderedQueryable<T>).Skip((page - 1) * pageSize).Take(pageSize);
+        {
+            if (page <= 0)
+                throw new ArgumentOutOfRangeException(nameof(page), "The given parameter can not be zero or negative.");
+            if (page <= 0)
+                throw new ArgumentOutOfRangeException(nameof(pageSize), "The given parameter can not be zero or negative.");
+
+            return (source as IOrderedQueryable<T>).Skip((page - 1) * pageSize).Take(pageSize);
+        }
     }
 }
