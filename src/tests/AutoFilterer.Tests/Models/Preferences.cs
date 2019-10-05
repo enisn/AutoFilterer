2742b2fcdb16b71f75652de7669b8032d6fd4628
Enis Necipoğlu <enis.necipoglu@outlook.com>
2019-10-05 20:01:34 +0300

    Add Tests

---
+++ src/tests/AutoFilterer.Tests/Models/Preferences.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFilterer.Tests.Models
+{
+    public class Preferences
+    {
+        public Guid UserId { get; set; }
+        public bool IsTwoFactorEnabled { get; set; }
+        public string GivenName { get; set; }
+        public int SecurityLevel { get; set; }
+    }
+}
