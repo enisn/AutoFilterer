e16d24867fcd637314676bf5a4aef103ba9db282
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-03-28 00:38:00 +0300

    update version to v1.2.0

--- src/AutoFilterer/Attributes/OperatorComparisonAttribute.cs
+++ src/AutoFilterer/Attributes/OperatorComparisonAttribute.cs
@@ -1,11 +1,6 @@
 ﻿using AutoFilterer.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoFilterer.Attributes
 {
