bcde6e5cab8b9fae5dbaec93c3b14c67e4eca3ba
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-04-18 01:26:39 +0300

    Make Comparison parameter as optional - Some of Entity Framework providers can not convert query when use 2 parameter string methods

--- src/AutoFilterer/Enums/StringFilterOption.cs
+++ src/AutoFilterer/Enums/StringFilterOption.cs
@@ -11,7 +11,5 @@ namespace AutoFilterer.Enums
         StartsWith = 1 << 0,
         EndsWith = 1 << 1,
         Contains = 1 << 2,
-        GreaterThan = 1 << 3,
-        LessThan = 1 << 4
     }
 }
