a6458ff4ce93615fc98bf97f0015448f5fc23331
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-05-16 19:03:48 +0300

    Add operatorfilter

---
+++ src/AutoFilterer/Types/OperatorFilter.cs
@@ -0,0 +1,69 @@
+﻿using AutoFilterer.Abstractions;
+using AutoFilterer.Attributes;
+using AutoFilterer.Enums;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AutoFilterer.Types
+{
+    public class OperatorFilter<T> : IFilterableType
+        where T : struct
+    {
+        public T? Eq { get; set; }
+        public T? Not { get; set; }
+        public T? Gt { get; set; }
+        public T? Lt { get; set; }
+        public T? Gte { get; set; }
+        public T? Lte { get; set; }
+
+        public CombineType CombineWith { get; set; }
+        public Expression BuildExpression(Expression expressionBody, PropertyInfo targetProperty, PropertyInfo filterProperty, object value)
+        {
+            Expression expression = null;
+
+            if (Eq != null)
+                expression = Combine(expression, new OperatorComparisonAttribute(OperatorType.Equal).BuildExpression(expressionBody, targetProperty, filterProperty, Eq));
+
+            if (Gt != null)
+                expression = Combine(expression, new OperatorComparisonAttribute(OperatorType.GreaterThan).BuildExpression(expressionBody, targetProperty, filterProperty, Gt));
+
+            if (Lt != null)
+                expression = Combine(expression, new OperatorComparisonAttribute(OperatorType.LessThan).BuildExpression(expressionBody, targetProperty, filterProperty, Lt));
+
+            if (Gte != null)
+                expression = Combine(expression, new OperatorComparisonAttribute(OperatorType.GreaterThanOrEqual).BuildExpression(expressionBody, targetProperty, filterProperty, Gte));
+
+            if (Lte != null)
+                expression = Combine(expression, new OperatorComparisonAttribute(OperatorType.LessThanOrEqual).BuildExpression(expressionBody, targetProperty, filterProperty, Lte));
+
+            if (Not != null)
+                expression = Combine(expression, new OperatorComparisonAttribute(OperatorType.NotEqual).BuildExpression(expressionBody, targetProperty, filterProperty, Not));
+
+            return expression;
+        }
+
+        private protected virtual Expression Combine(Expression left, Expression right)
+        {
+            if (left == null)
+                return right;
+            if (right == null)
+                return left;
+
+            if (left is ParameterExpression || left is MemberExpression)
+                return right;
+            if (right is ParameterExpression || right is MemberExpression)
+                return left;
+
+            switch (this.CombineWith)
+            {
+                case CombineType.And:
+                    return Expression.And(left, right);
+                case CombineType.Or:
+                    return Expression.Or(left, right);
+                default:
+                    return right;
+            }
+        }
+    }
+}
---
+++ tests/AutoFilterer.Tests/Types/OperatorQueryTests.cs
@@ -0,0 +1,285 @@
+﻿using AutoFilterer.Extensions;
+using AutoFilterer.Tests.Envirorment.Dtos;
+using AutoFilterer.Tests.Envirorment.Models;
+using AutoFilterer.Tests.Envirorment.Statics;
+using AutoFilterer.Types;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AutoFilterer.Tests.Types
+{
+    public class OperatorQueryTests
+    {
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageWithAnd_SholdMatchCount(List<Book> data, BookFilter_OperatorFilter_TotalPage filter)
+        {
+            // Arrange
+            filter.CombineWith = Enums.CombineType.And;
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var f = filter;
+            var actualResult = GetAndQuery(data.AsQueryable(), filter).ToList();
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+            
+            IQueryable<Book> GetAndQuery(IQueryable<Book> query, BookFilter_OperatorFilter_TotalPage f)
+            {
+                if (f.TotalPage.Eq != null)
+                    query = query.Where(x => x.TotalPage == f.TotalPage.Eq);
+
+                if (f.TotalPage.Not != null)
+                    query = query.Where(x => x.TotalPage != f.TotalPage.Not);
+
+                if (f.TotalPage.Gt != null)
+                    query = query.Where(x => x.TotalPage > f.TotalPage.Gt);
+
+                if (f.TotalPage.Gte != null)
+                    query = query.Where(x => x.TotalPage >= f.TotalPage.Gte);
+
+                if (f.TotalPage.Lt!= null)
+                    query = query.Where(x => x.TotalPage < f.TotalPage.Lt);
+
+                if (f.TotalPage.Lte!= null)
+                    query = query.Where(x => x.TotalPage <= f.TotalPage.Lt);
+
+                return query;
+            }
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageEqWithAnd_ShouldMatchCount(List<Book> data, int totalPage)
+        {
+            // Arrange
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Eq = totalPage, 
+                    CombineWith = Enums.CombineType.And
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage == totalPage).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageNotWithAnd_ShouldMatchCount(List<Book> data, int totalPage)
+        {
+            // Arrange
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Not = totalPage, 
+                    CombineWith = Enums.CombineType.And
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage != totalPage).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageGtWithAnd_ShouldMatchCount(List<Book> data, int totalPage)
+        {
+            // Arrange
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Gt = totalPage, 
+                    CombineWith = Enums.CombineType.And
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage > totalPage).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageGteWithAnd_ShouldMatchCount(List<Book> data, int totalPage)
+        {
+            // Arrange
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Gte = totalPage, 
+                    CombineWith = Enums.CombineType.And
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage >= totalPage).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageLteWithAnd_ShouldMatchCount(List<Book> data, int totalPage)
+        {
+            // Arrange
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Lte = totalPage, 
+                    CombineWith = Enums.CombineType.And
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage <= totalPage).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageLtWithAnd_ShouldMatchCount(List<Book> data, int totalPage)
+        {
+            // Arrange
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Lt = totalPage, 
+                    CombineWith = Enums.CombineType.And
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage < totalPage).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageLtGtWithAnd_ShouldMatchCount(List<Book> data, int min, int some)
+        {
+            // Arrange
+            var max = min + some;
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Gt = min,
+                    Lt = max, 
+                    CombineWith = Enums.CombineType.And
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage > min && x.TotalPage < max).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageLtGtWithOr_ShouldMatchCount(List<Book> data, int min, int some)
+        {
+            // Arrange
+            var max = min + some;
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Gt = min,
+                    Lt = max, 
+                    CombineWith = Enums.CombineType.Or
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage > min || x.TotalPage < max).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+
+        [Theory, AutoMoqData(count: 64)]
+        public void BuildExpression_TotalPageLtEqWithOr_ShouldMatchCount(List<Book> data, int max, int exact)
+        {
+            // Arrange
+            var filter = new BookFilter_OperatorFilter_TotalPage
+            {
+                TotalPage = new OperatorFilter<int>
+                {
+                    Eq = exact,
+                    Lt = max, 
+                    CombineWith = Enums.CombineType.Or
+                }
+            };
+
+            // Act
+            var query = data.AsQueryable().ApplyFilter(filter);
+            var result = query.ToList();
+
+            // Assert
+            var actualResult = data.AsQueryable().Where(x => x.TotalPage < max || x.TotalPage == exact).ToList();
+
+            Assert.Equal(actualResult.Count, result.Count);
+            foreach (var item in actualResult)
+                Assert.Contains(item, result);
+        }
+    }
+}
