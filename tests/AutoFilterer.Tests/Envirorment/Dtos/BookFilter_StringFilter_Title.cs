2f7524403da9e2a64a2abfd30ce2fe30946551dd
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-05-16 19:34:13 +0300

    Add StringFilter type

---
+++ tests/AutoFilterer.Tests/Envirorment/Dtos/BookFilter_StringFilter_Title.cs
@@ -0,0 +1,9 @@
+﻿using AutoFilterer.Types;
+
+namespace AutoFilterer.Tests.Envirorment.Dtos
+{
+    public class BookFilter_StringFilter_Title : FilterBase
+    {
+        public StringFilter Title { get; set; }
+    }
+}
