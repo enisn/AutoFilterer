75b04ad4088469b5648091424bab88b3a1ef7177
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-05-17 16:17:57 +0300

    Add AutoFilterer.Swagger extensions

---
+++ src/AutoFilterer.Swagger/OperationFilters/OrderableEnumOperationFilter.cs
@@ -0,0 +1,39 @@
+﻿using AutoFilterer.Abstractions;
+using AutoFilterer.Attributes;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+using System.Reflection;
+
+namespace AutoFilterer.Swagger.OperationFilters
+{
+    public class OrderableEnumOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            foreach (var item in operation.Parameters)
+            {
+                var param = context.ApiDescription.ParameterDescriptions.FirstOrDefault(x => x.Name == item.Name);
+
+                if (typeof(IOrderable).IsAssignableFrom(param.ModelMetadata.ContainerType) && param.Name == nameof(IOrderable.Sort))
+                {
+                    foreach (var prop in param.ModelMetadata.ContainerType.GetProperties())
+                        if (!prop.HasAttribute<IgnoreFilterAttribute>())
+                        {
+                            var compareTo = prop.GetCustomAttribute<CompareToAttribute>();
+                            if (compareTo != null)
+                            {
+                                foreach (var propertyName in compareTo.PropertyNames)                                
+                                    item.Schema.Enum.Add(new OpenApiString(propertyName));                                
+                            }
+                            else
+                            {
+                                item.Schema.Enum.Add(new OpenApiString(prop.Name));
+                            }
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ src/AutoFilterer.Swagger/Startup.cs
@@ -0,0 +1,20 @@
+﻿using AutoFilterer.Swagger.OperationFilters;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFilterer.Swagger
+{
+    public static class Startup
+    {
+        public static SwaggerGenOptions AddAutoFilterer(this SwaggerGenOptions options)
+        {
+            options.OperationFilter<OrderableEnumOperationFilter>();
+            return options;
+        }
+    }
+}
