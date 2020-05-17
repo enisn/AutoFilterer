2e8bf963f2a1ceb0e92673610bc49f5075583a64
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-05-17 16:17:04 +0300

    Update sample

--- sandbox/WebApplication.API/Dtos/BookFilter.cs
+++ sandbox/WebApplication.API/Dtos/BookFilter.cs
@@ -2,10 +2,6 @@
 using AutoFilterer.Enums;
 using AutoFilterer.Types;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebApplication.API.Models;
 
 namespace WebApplication.API.Dtos
