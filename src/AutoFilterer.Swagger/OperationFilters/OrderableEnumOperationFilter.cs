ac7aaf88c2e4c7fdfeb7ea6e98a3eed90b2fe158
enisn <iletisim@enisnecipoglu.com>
2020-11-01 17:48:50 +0300

    ♻ Refactor OrderableEnumOperationFilter

--- src/AutoFilterer.Swagger/OperationFilters/OrderableEnumOperationFilter.cs
+++ src/AutoFilterer.Swagger/OperationFilters/OrderableEnumOperationFilter.cs
@@ -1,5 +1,6 @@
 ﻿using AutoFilterer.Abstractions;
 using AutoFilterer.Attributes;
+using AutoFilterer.Types;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -29,26 +30,33 @@ namespace AutoFilterer.Swagger.OperationFilters
 
                     foreach (var prop in param.ModelMetadata.ContainerType.GetProperties().Where(x => !x.HasAttribute<IgnoreFilterAttribute>()))
                     {
-                        var compareTo = prop.GetCustomAttribute<CompareToAttribute>();
-                        if (compareTo != null)
-                        {
-                            foreach (var propertyName in compareTo.PropertyNames)
-                                item.Schema.Enum.Add(new OpenApiString(propertyName));
-                        }
-                        else if (typeof(IFilter).IsAssignableFrom(prop.PropertyType))
-                        {
-                            foreach (var innerProp in prop.PropertyType.GetProperties())
-                            {
-                                item.Schema.Enum.Add(new OpenApiString(prop.Name + "." + innerProp.Name));
-                            }
-                        }
-                        else
-                        {
-                            item.Schema.Enum.Add(new OpenApiString(prop.Name));
-                        }
+                        AddPropertyAsEnum(item, prop);
                     }
                 }
             }
         }
+
+        private static void AddPropertyAsEnum(OpenApiParameter item, PropertyInfo prop, string aggragatedName = default)
+        {
+            var compareTo = prop.GetCustomAttribute<CompareToAttribute>();
+            if (compareTo != null)
+            {
+                foreach (var propertyName in compareTo.PropertyNames)
+                    if (IsValidPropertyToOrder(propertyName))
+                        item.Schema.Enum.Add(new OpenApiString((aggragatedName + "." + propertyName).Trim('.')));
+            }
+            else if (typeof(IFilter).IsAssignableFrom(prop.PropertyType))
+            {
+                foreach (var innerProp in prop.PropertyType.GetProperties())
+                    if (IsValidPropertyToOrder(innerProp.Name))
+                        AddPropertyAsEnum(item, innerProp, (aggragatedName + "." + prop.Name).Trim('.'));
+            }
+            else if(IsValidPropertyToOrder(prop.Name))
+            {
+                item.Schema.Enum.Add(new OpenApiString((aggragatedName + "." + prop.Name).Trim('.')));
+            }
+        }
+
+        private static bool IsValidPropertyToOrder(string propertyName) => !typeof(PaginationFilterBase).GetProperties().Any(a => a.Name == propertyName);
     }
 }
