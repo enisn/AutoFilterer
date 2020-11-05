86a4a7d0554468c35a831e7d59027591f4c5716b
furkandeveloper <furkan.dvlp@gmail.com>
2020-11-05 14:53:59 +0300

    🔨 Add 'LowerContains' attribute. ✨ This class generates contains query, for string fields.

---
+++ src/AutoFilterer/Attributes/ToLowerContainsComparisonAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace AutoFilterer.Attributes
+{
+    /// <summary>
+    /// This class generates contains query, for string fields.
+    /// </summary>
+    public class ToLowerContainsComparisonAttribute : FilteringOptionsBaseAttribute
+    {
+        public override Expression BuildExpression(Expression expressionBody, PropertyInfo targetProperty, PropertyInfo filterProperty, object value)
+        {
+            var containsMethod = typeof(string).GetMethod(nameof(string.Contains), types: new[] { typeof(string) });
+
+            var toLowerMethod = typeof(string).GetMethod(nameof(string.ToLower), types: new Type[0]);
+
+            var comparison = Expression.Equal(
+                        Expression.Call(
+                            method: containsMethod,
+                            instance: Expression.Call(method: toLowerMethod, instance: Expression.Property(expressionBody, targetProperty.Name)
+                                ),
+                            arguments: new[] { Expression.Call(method: toLowerMethod, instance: Expression.Constant(value)) }),
+                        Expression.Constant(true));
+
+            return comparison;
+        }
+    }
+}
