7f9057beb8a9b36309137a669a89c5813d0f9f8d
enisn <iletisim@enisnecipoglu.com>
2020-11-20 15:37:39 +0300

    ✨ Abstracting Ordering & Pagination seperately

---
+++ src/AutoFilterer/Abstractions/IOrderablePaginationFilter.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace AutoFilterer.Abstractions
+{
+    public interface IOrderablePaginationFilter : IPaginationFilter, IOrderable, IFilter
+    {
+        IQueryable<T> ApplyFilterWithoutPaginationAndOrdering<T>(IQueryable<T> query);
+    }
+}
---
+++ src/AutoFilterer/Abstractions/IPaginationFilter.cs
@@ -0,0 +1,13 @@
+﻿using AutoFilterer.Attributes;
+using System.Linq;
+
+namespace AutoFilterer.Abstractions
+{
+    public interface IPaginationFilter : IFilter
+    {
+        [IgnoreFilter] int Page { get; set; }
+        [IgnoreFilter] int PerPage { get; set; }
+
+        IQueryable<T> ApplyFilterWithoutPagination<T>(IQueryable<T> query);
+    }
+}
