838ac066e5bedd9250e13daa09b4c44c2de3a687
enisn <iletisim@enisnecipoglu.com>
2020-12-02 00:51:07 +0300

    🔥 Remove generic PaginationFilterBase

--- src/AutoFilterer/Types/PaginationFilterBase.cs
+++ src/AutoFilterer/Types/PaginationFilterBase.cs
@@ -32,23 +32,4 @@ namespace AutoFilterer.Types
             => base.ApplyFilterWithoutOrdering(query);
 
     }
-
-    [Obsolete("This is deprecated. Use PaginationFilterBase instead of PaginationFilterBase<T>. Just remove Type parameter from this.", true)]
-    public class PaginationFilterBase<T> : FilterBase<T>
-    {
-        [IgnoreFilter]
-        public virtual int Page { get; set; } = 1;
-
-        [IgnoreFilter]
-        public virtual int PerPage { get; set; } = 10;
-
-        public virtual IQueryable<T> ApplyFilterTo(IOrderedQueryable<T> query)
-            => base.ApplyFilterTo(query).ToPaged(Page, PerPage);
-
-        public virtual IQueryable<TEntity> ApplyFilterTo<TEntity>(IOrderedQueryable<TEntity> query)
-            => base.ApplyFilterTo(query).ToPaged(Page, PerPage);
-
-        public IQueryable<T> ApplyFilterWithoutPagination<T>(IQueryable<T> query)
-            => base.ApplyFilterTo(query);
-    }
 }
