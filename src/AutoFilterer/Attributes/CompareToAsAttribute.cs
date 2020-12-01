1dc28e58a776ad5ee593eb8141d079aaa3b6e1a2
enisn <iletisim@enisnecipoglu.com>
2020-12-02 02:05:23 +0300

    ✨ Add CompareToAsAttribute

---
+++ src/AutoFilterer/Attributes/CompareToAsAttribute.cs
@@ -0,0 +1,43 @@
+﻿using AutoFilterer.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFilterer.Attributes
+{
+    public class CompareToAsAttribute : CompareToAttribute
+    {
+        private Type filterAttributeType;
+
+        public CompareToAsAttribute(Type filterAttributeType, params string[] propertyNames) : base(propertyNames)
+        {
+            FilterAttributeType = filterAttributeType;
+        }
+
+        public CompareToAsAttribute(Type filterAttributeType, CombineType combineWith, params string[] propertyNames) : base(combineWith, propertyNames)
+        {
+            FilterAttributeType = filterAttributeType;
+        }
+
+        public Type FilterAttributeType { get => filterAttributeType; set => SetFilterAttributeType(value); }
+        public IPropertyExpressionable Comparer { get; private set; }
+
+        private void SetFilterAttributeType(Type type)
+        {
+            if (!typeof(IPropertyExpressionable).IsAssignableFrom(type))
+                throw new ArgumentException();
+
+            this.Comparer = (IPropertyExpressionable)Activator.CreateInstance(type);
+            filterAttributeType = type;
+        }
+
+        public override Expression BuildExpression(Expression expressionBody, PropertyInfo targetProperty, PropertyInfo filterProperty, object value)
+        {
+            return Comparer.BuildExpression(expressionBody, targetProperty, filterProperty, value);
+        }
+    }
+}
