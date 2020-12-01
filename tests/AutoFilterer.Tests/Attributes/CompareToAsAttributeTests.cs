73f5b8ea394f31716298e0e043dca9fce57ebb23
enisn <iletisim@enisnecipoglu.com>
2020-12-02 02:05:41 +0300

    🧪 Add CompareToAsAttribute Tests

---
+++ tests/AutoFilterer.Tests/Attributes/CompareToAsAttributeTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoFilterer.Extensions;
+using AutoFilterer.Tests.Environment.Dtos;
+using AutoFilterer.Tests.Environment.Models;
+using AutoFilterer.Tests.Environment.Statics;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AutoFilterer.Tests.Attributes
+{
+    public class CompareToAsAttributeTests
+    {
+        [Theory, AutoMoqData(64)]
+        public void BuildExpression_MultipleFieldWithToLowerContainsAttribute_ShouldWorkProperly(List<Book> dummyData)
+        {
+            // Arrange
+            var filter = new BookFilter_CompareToAs_ToLowerContains
+            {
+                Query = "a",
+                CombineWith = CombineType.Or
+            };
+
+            var query = dummyData.AsQueryable();
+            var expectedQuery = query.Where(x => 
+                                    x.Title.ToLower().Contains("a".ToLower()) == true
+                                    ||
+                                    x.Author.ToLower().Contains("a".ToLower()) == true
+                                    );
+            var expectedResult = expectedQuery.ToList();
+
+            // Act
+            var actualQuery = query.ApplyFilter(filter);
+            var actualResult = actualQuery.ToList();
+
+            // Assert
+            Assert.Equal(expectedQuery.ToString(), actualQuery.ToString());
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ tests/AutoFilterer.Tests/Environment/Dtos/BookFilter_CompareToAs_ToLowerContains.cs
@@ -0,0 +1,17 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Tests.Environment.Models;
+using AutoFilterer.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFilterer.Tests.Environment.Dtos
+{
+    public class BookFilter_CompareToAs_ToLowerContains : FilterBase
+    {
+        [CompareToAs(typeof(ToLowerContainsComparisonAttribute), nameof(Book.Title), nameof(Book.Author))]
+        public string Query { get; set; }
+    }
+}
