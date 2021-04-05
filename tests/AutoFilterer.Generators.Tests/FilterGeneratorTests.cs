05411b2322334222bbbb6be5c9a8a2f1e486a431
enisn <info@enisnecipoglu.com>
2021-04-05 14:50:33 +0300

    ✅  Add test for TypeMappings

--- tests/AutoFilterer.Generators.Tests/FilterGeneratorTests.cs
+++ tests/AutoFilterer.Generators.Tests/FilterGeneratorTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoFilterer.Enums;
 using Xunit;
 
 namespace AutoFilterer.Generators.Tests
@@ -60,9 +61,48 @@ namespace AutoFilterer.Generators.Tests
             filter.Page = 1;
             filter.PerPage = 2;
             filter.Year = new Types.Range<int>(min: 1990, max: 2021);
-
-
+            
             books.AsQueryable().ApplyFilter(filter);
+        }
+        
+        [AutoFilterDto("MappingTest")]
+        public class AllTypesTestType
+        {
+            public sbyte _Sbyte { get; set; }
+            public sbyte? _SbyteN { get; set; }
+            public byte _Byte { get; set; }  
+            public byte? _ByteN { get; set; }
+            public short _Short { get; set; }  
+            public short? _ShortN { get; set; }
+            public ushort _Ushort { get; set; }
+            public ushort? _UshortN { get; set; }
+            public int _Int { get; set; }
+            public int?  _IntN { get; set; }
+            public uint _UInt { get; set; }
+            public uint? _UIntN { get; set; }
+            public long _Long { get; set; }
+            public long? _LongN { get; set; }
+            public ulong _ULong { get; set; }
+            public ulong? _ULongN { get; set; }
+            public double _Double { get; set; }
+            public double? _DoubleN { get; set; }
+            public float _Float { get; set; }
+            public float? _FloatN { get; set; }
+            public decimal _Decimal { get; set; }
+            public decimal? _DecimalN { get; set; }
+            public DateTime _DateTime { get; set; }
+            public DateTime? _DateTimeN { get; set; }
+            public TimeSpan _TimeSpan { get; set; }
+            public TimeSpan? _TimeSpanN { get; set; }
+        }
+
+        [Fact]
+        public void ShouldCreateEachTypeCorrectFromMapping()
+        {
+            Assert.NotNull(typeof(MappingTest.AllTypesTestTypeFilter));
+            
+            var filter = new MappingTest.AllTypesTestTypeFilter();
         }
     }
 }
+
