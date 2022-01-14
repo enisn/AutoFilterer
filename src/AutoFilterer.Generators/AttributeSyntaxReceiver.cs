4a4dcd61c12b65e1570406f9d95e216448a025ec
enisn <info@enisnecipoglu.com>
2022-01-14 22:40:46 +0300

    Fix duplicated generated code in Generators

---
+++ src/AutoFilterer.Generators/AttributeSyntaxReceiver.cs
@@ -0,0 +1,26 @@
+﻿using AutoFilterer.Generators.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNurse.CodeAnalysis;
+
+public class AttributeSyntaxReceiver<TAttribute> : ISyntaxReceiver
+       where TAttribute : Attribute
+{
+    public IList<ClassDeclarationSyntax> Classes { get; } = new List<ClassDeclarationSyntax>();
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is ClassDeclarationSyntax classDeclarationSyntax &&
+            classDeclarationSyntax.AttributeLists.Count > 0 &&
+            classDeclarationSyntax.AttributeLists
+                .Any(al => al.Attributes
+                    .Any(a => a.Name.ToString().EnsureEndsWith("Attribute").Equals(typeof(TAttribute).Name))))
+        {
+            Classes.Add(classDeclarationSyntax);
+        }
+    }
+}
